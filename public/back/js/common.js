efe1784c4592613355065da4f51d0920d977c1ca
jiyuanyuan <1125111926@qq.com>
2017-11-21 19:37:38 +0800

    完成了登录功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,16 @@
 /**
  * Created by Administrator on 2017/11/21.
  */
+
+NProgress.configure({ showSpinner:false });
+$(document).ajaxStart(function () {
+    //开启进度条
+    NProgress.start();
+});
+$(document).ajaxStop(function () {
+    //完成进度条
+    setTimeout(function () {
+        NProgress.done();
+    },500);
+
+});
